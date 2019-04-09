395b5bc28596663a1cf44cd5da34a1652b7bbc63
ya.zhang <zhangya3@mychery.com>
2019-04-09 16:28:51 +0800

    Rxjava的create实现步骤

--- rxjavademo/src/main/java/com/example/rxjavademo/MainActivity.java
+++ rxjavademo/src/main/java/com/example/rxjavademo/MainActivity.java
@@ -2,12 +2,96 @@ package com.example.rxjavademo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getTypeName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * 1.第一种方式
+         * Rxjava基本的分步骤实现方式
+         *
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>(){
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onComplete();
+            }
+        });
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用onSubscribe连接");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: 对"+integer+"做出反应");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+
+        observable.subscribe(observer);.
+         */
+
+        /**
+         * 2.第二种方式
+         * RxJava的链式调用
+         *
+         */
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(4);
+                e.onNext(5);
+                e.onNext(6);
+                e.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用onSubscribe2连接");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: 对"+integer+"做出反应");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+
     }
 }
