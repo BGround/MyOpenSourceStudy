cf9e28ffa99a3bd1dbc020c7e4e480e5b2295281
ya.zhang <zhangya3@mychery.com>
2019-04-12 16:05:27 +0800

    组合操作符demo

---
+++ rxjavademo/src/main/java/com/example/rxjavademo/demo/CombineOperatorDemo1.java
@@ -0,0 +1,19 @@
+package com.example.rxjavademo.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rxjavademo.R;
+
+
+public class CombineOperatorDemo1 extends AppCompatActivity {
+    private final String TAG = getClass().getTypeName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
